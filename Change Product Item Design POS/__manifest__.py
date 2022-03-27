00c49be86d220a7438e9d2a050acb9b566e33793
Ahmed Salah <GeekAhmeds@users.noreply.github.com>
2022-03-27 21:23:38 +0200

    Update __manifest__.py

--- Change Product Item Design POS/__manifest__.py
+++ Change Product Item Design POS/__manifest__.py
@@ -26,7 +26,7 @@ Large Name without image.
     'category': 'Point Of Sale',
     'support': 'info@th3company.com',
     'version': '15.0.0.1',
-    'images': ['static/description/images/screen/2.png'],
+    'images': ['static/description/banner.png'],
     # any module necessary for this one to work correctly
     'depends': ['point_of_sale'],
     'assets': {
